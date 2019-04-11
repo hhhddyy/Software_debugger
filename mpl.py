6668270dc13f6a8f2544d73bd7f8a414cf23b172
Ddsluke <35838366+Ddsluke@users.noreply.github.com>
2019-04-11 21:06:34 +0800

    Big Update!!
    cmp_error unfinished

--- mpl.py
+++ mpl.py
@@ -2,46 +2,54 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def draw_plots(x, y1, y2, ErrorType):
-    fig = plt.figure(figsize=(14,5))
-    
-    l1 = plt.plot(x,y1,color='blue',label="Tarantula")
-    l2 = plt.plot(x,y2,color='red',label="Crosstab")
+def cmp_debugger(tarantula, crosstab, ErrorType):
+    x = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+    fig = plt.figure(figsize=(14, 5))
 
-    plt.title('Debugger Comparison (' + ErrorType + ')')
-    
-    plt.xlim((-10,100))
-    plt.ylim((0,1))
-    
-    plt.xlabel("SCORE")
-    plt.ylabel("Percentage")
-    
-    plt.legend(loc = 'upper left')
-    
-    ##x_ticks = np.linspace(0,90,10) # set ticks for X
-    ##plt.xticks(x_ticks)
+    l1 = plt.plot(x, tarantula, color='blue', label="Tarantula", marker="+")
+    l2 = plt.plot(x, crosstab, color='red', label="Crosstab", marker="x")
+
+    plt.title('Debugger Comparison (' + str(ErrorType) + ')')
+
+    plt.xlim((0, 100))
+    plt.ylim((0, 105))
+
+    plt.xlabel("SCORE (% of program need not to be examined)")
+    plt.ylabel("% of test runs")
+
+    plt.legend(loc='lower right')
+
+    #  x_ticks = np.linspace(0,90,10) # set ticks for X
+    #  plt.xticks(x_ticks)
     x_tick = []
-    for i in range(10):
-        x_tick.append(str(i*10) + r"%-" + str(i*10 + 10) + "%")
-    plt.xticks([0,10,20,30,40,50,60,70,80,90], x_tick) # set ticks for X
-    y_ticks = np.linspace(0,1,11)
+    for i in range(11):
+        #  x_tick.append(str(i * 10) + r"%-" + str(i * 10 + 10) + "%")  #  This list is to print str like "0%-10%"
+        x_tick.append(str(100 - i * 10) + "%")
+    plt.xticks([0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100], x_tick)  # set ticks for X
+    y_ticks = np.linspace(0, 100, 11)
     plt.yticks(y_ticks)
 
-    for a,b in zip(x, y1):
+    for a, b in zip(x, tarantula):
         plt.text(a, b, b, ha='center', va='bottom', color='blue')
-    for a,b in zip(x, y2):
+    for a, b in zip(x, crosstab):
         plt.text(a, b, b, ha='center', va='bottom', color='red')
-    plt.savefig(r'debugger.png') # after plt.show() is called, a new figure is created
-    plt.show()
+    plt.savefig(r'debugger.png')  # after plt.show() is called, a new figure is created
+    #  plt.show()
+
+
+def cmp_error(data, num, debugger): #  data is a list of lists containing the data, num is the number of error types
+    #  debugger is the type of debugger (str)
+    x = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+    fig = plt.figure(figsize=(14, 5))
 
 
 def main():
-    ErrorType = "Unknown ErrorType"
-    x = [0,10,20,30,40,50,60,70,80,90]
-    y1 = [0.1,0.1,0.5,0.5,0.2,0.9,0.6,0.3,0.4,0.6]
-    y2 = [0.5,0.1,0.6,0.2,0.3,0.7,0.8,0.2,0.2,0.9]
+    #  The tarantula crosstab here are only for testing. It is irrelevant to the debuggers.
+    ErrorType = "ErrorType"
+    tarantula = [10, 10, 20, 30, 60, 80, 90, 100, 100, 100, 100]
+    crosstab = [10, 20, 30, 35, 45, 70, 85, 95, 100, 100, 100]
     # The variables above should be generated from summarize.py
-    draw_plots(x, y1, y2, ErrorType)
+    cmp_debugger(tarantula, crosstab, ErrorType)
 
 
 if __name__ == '__main__':
--- summarize.py
+++ summarize.py
@@ -1,27 +1,33 @@
 import pandas as pd
 import mpl
-import matplotlib.pyplot as plt
 
 
 def read_data(path):
     df = pd.read_excel(path, header=None)
+    score_distribution = [0] * 11  # The list currently represents the total frequency of testcases whose score is in
+    #  index * 10% - (index+1) * 10%
     count = 0  # Updated to 0 once the line_no has reached a new test case
     line_no = 0
     result_num = 0
     test_sum = 0
-    df.columns = ["Statement", "Suspiciousness", "Bug", "Frequency"]
+    df.columns = ["Statement", "Suspiciousness", "Bug", "Frequency", "ErrorType", "LOC"]
+    LOC = int(df.loc[1, "LOC"])
+    ErrorType = df.loc[1, "ErrorType"]
     try:
         while True:
             if df.loc[line_no, "Statement"] == "Statement":
                 result_num += 1
                 count = 0
-                bug = df.loc[line_no+1, "Bug"]
-                freq = df.loc[line_no+1, "Frequency"]
+                bug = int(df.loc[line_no+1, "Bug"])
+                freq = int(df.loc[line_no+1, "Frequency"])
                 test_sum += freq
                 print("Result " + str(result_num) + ":" + " Bug appears at line " + str(bug) + "." + " Frequency is " +
                       str(freq) + ".")
             elif df.loc[line_no, "Statement"] == bug:
                 print("The number of statements we need to examine is: " + str(count))
+                score = float(LOC - count) / float(LOC)
+                index = 10 - int(score * 10)
+                score_distribution[index] += freq
             else:
                 count += 1
 
@@ -29,11 +35,23 @@ def read_data(path):
 
     except KeyError:  # KeyError is for loc, IndexError is for iloc
         print("All testcases read. The total number of testcases is:", test_sum)
+    dst = [round(a/test_sum, 2) for a in score_distribution]
+    sum = 0
+    result = []
+    for i in range(11):
+        sum += dst[i] * 100
+        result.append(sum)
+    return result, ErrorType
 
 
 def main():
     path = "result.xlsx"
-    read_data(path)
+    path2 = "result1.xlsx"
+    y1tuple = read_data(path)
+    y1 = y1tuple[0]
+    errortype1 = y1tuple[1]
+    y2 = read_data(path2)[0]
+    mpl.cmp_debugger(y1, y2, errortype1)
 
 
 if __name__ == "__main__":
