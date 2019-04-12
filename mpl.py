b6891b200cf0f4743fa9c72d71bdfe7588d0d6d9
Ddsluke <35838366+Ddsluke@users.noreply.github.com>
2019-04-12 21:19:49 +0800

    Update!!!

--- mpl.py
+++ mpl.py
@@ -2,26 +2,28 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def cmp_debugger(tarantula, crosstab, ErrorType):
+def cmp(data, labels, num, title, path):
+    #  data is a list of lists containing the data, num is the number of error types
+    #  debugger is the type of debugger (str)
+    colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k']
     x = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
     fig = plt.figure(figsize=(14, 5))
-
-    l1 = plt.plot(x, tarantula, color='blue', label="Tarantula", marker="+")
-    l2 = plt.plot(x, crosstab, color='red', label="Crosstab", marker="x")
-
-    plt.title('Debugger Comparison (' + str(ErrorType) + ')')
-
-    plt.xlim((0, 100))
-    plt.ylim((0, 105))
+    plt.title(title)
 
     plt.xlabel("SCORE (% of program need not to be examined)")
     plt.ylabel("% of test runs")
 
+    plt.xlim((0, 100))
+    plt.ylim((0, 105))
+
+    for i in range(num):
+        plt.plot(x, data[i], color=colors[i], label=labels[i], marker="x")
+        for a, b in zip(x, data[i]):
+            plt.text(a, b, b, ha='center', va='bottom', color=colors[i])
+
     plt.legend(loc='lower right')
 
-    #  x_ticks = np.linspace(0,90,10) # set ticks for X
-    #  plt.xticks(x_ticks)
-    x_tick = []
+    x_tick = list()
     for i in range(11):
         #  x_tick.append(str(i * 10) + r"%-" + str(i * 10 + 10) + "%")  #  This list is to print str like "0%-10%"
         x_tick.append(str(100 - i * 10) + "%")
@@ -29,27 +31,24 @@ def cmp_debugger(tarantula, crosstab, ErrorType):
     y_ticks = np.linspace(0, 100, 11)
     plt.yticks(y_ticks)
 
-    for a, b in zip(x, tarantula):
-        plt.text(a, b, b, ha='center', va='bottom', color='blue')
-    for a, b in zip(x, crosstab):
-        plt.text(a, b, b, ha='center', va='bottom', color='red')
-    plt.savefig(r'debugger.png')  # after plt.show() is called, a new figure is created
-    #  plt.show()
-
-
-def cmp_error(data, num, debugger): #  data is a list of lists containing the data, num is the number of error types
-    #  debugger is the type of debugger (str)
-    x = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
-    fig = plt.figure(figsize=(14, 5))
+    plt.savefig(path)
 
 
 def main():
-    #  The tarantula crosstab here are only for testing. It is irrelevant to the debuggers.
-    ErrorType = "ErrorType"
-    tarantula = [10, 10, 20, 30, 60, 80, 90, 100, 100, 100, 100]
-    crosstab = [10, 20, 30, 35, 45, 70, 85, 95, 100, 100, 100]
+    # The tarantula & crosstab here are only for testing. They are irrelevant to the debuggers.
+    # ErrorType = "mergeSort"
+    # tarantula = [10, 10, 20, 30, 60, 80, 90, 100, 100, 100, 100]
+    # crosstab = [10, 20, 30, 35, 45, 70, 85, 95, 100, 100, 100]
     # The variables above should be generated from summarize.py
-    cmp_debugger(tarantula, crosstab, ErrorType)
+    # cmp_debugger(tarantula, crosstab, ErrorType)
+
+    # data = list()
+    # data.append([10, 15, 26, 37, 48, 70, 88, 100, 100, 100, 100])
+    # data.append([30, 45, 57, 68, 80, 96, 100, 100, 100, 100, 100])
+    # data.append([25, 44, 70, 90, 100, 100, 100, 100, 100, 100, 100])
+    # labels = ["boundary", "process", "computation"]
+    # cmp_error(data, labels, num=3, debugger="tarantula")
+    pass
 
 
 if __name__ == '__main__':
--- summarize.py
+++ summarize.py
@@ -18,8 +18,8 @@ def read_data(path):
             if df.loc[line_no, "Statement"] == "Statement":
                 result_num += 1
                 count = 0
-                bug = int(df.loc[line_no+1, "Bug"])
-                freq = int(df.loc[line_no+1, "Frequency"])
+                bug = int(df.loc[line_no + 1, "Bug"])
+                freq = int(df.loc[line_no + 1, "Frequency"])
                 test_sum += freq
                 print("Result " + str(result_num) + ":" + " Bug appears at line " + str(bug) + "." + " Frequency is " +
                       str(freq) + ".")
@@ -35,23 +35,58 @@ def read_data(path):
 
     except KeyError:  # KeyError is for loc, IndexError is for iloc
         print("All testcases read. The total number of testcases is:", test_sum)
-    dst = [round(a/test_sum, 2) for a in score_distribution]
+    return [score_distribution, test_sum]
+
+    # dst = [round(a/test_sum, 2) for a in score_distribution]
+    # sum = 0
+    # result = []
+    # for i in range(11):
+    #    sum += dst[i] * 100
+    #    result.append(sum)
+    # return result, ErrorType
+
+
+def integrate(*args):  # Each argument is a list [score_distribution, test_sum]
+    result = [[0] * 11, 0]
+    for i in args:
+        for j in range(11):
+            result[0][j] += i[0][j]
+        result[1] += i[1]
+
+    dst = [a/result[1] for a in result[0]]
+
+    output = list()
     sum = 0
-    result = []
     for i in range(11):
         sum += dst[i] * 100
-        result.append(sum)
-    return result, ErrorType
+        output.append(sum)
+    output = [round(a, 2) for a in output]
+    return output
 
 
 def main():
-    path = "result.xlsx"
-    path2 = "result1.xlsx"
-    y1tuple = read_data(path)
-    y1 = y1tuple[0]
-    errortype1 = y1tuple[1]
-    y2 = read_data(path2)[0]
-    mpl.cmp_debugger(y1, y2, errortype1)
+    tt1 = r"output/output/Tarantula/merge_computation.xlsx"
+    tt2 = r"output/output/Tarantula/merge_boundary.xlsx"
+    tt3 = r"output/output/Tarantula/merge_process.xlsx"
+    tt4 = r"output/output/Tarantula/select_computation.xlsx"
+    tt5 = r"output/output/Tarantula/select_boundary.xlsx"
+    tt6 = r"output/output/Tarantula/select_process.xlsx"
+
+    ct1 = r"output/output/crosstab/merge_computation.xlsx"
+    ct2 = r"output/output/crosstab/merge_boundary.xlsx"
+    ct3 = r"output/output/crosstab/merge_process.xlsx"
+    ct4 = r"output/output/crosstab/select_computation.xlsx"
+    ct5 = r"output/output/crosstab/select_boundary.xlsx"
+    ct6 = r"output/output/crosstab/select_process.xlsx"
+
+    # ct_merge = integrate(read_data(ct1), read_data(ct2), read_data(ct3))
+    # ct_select = integrate(read_data(ct4), read_data(ct5), read_data(ct6))
+    tt_merge_comp = integrate(read_data(tt3))
+    ct_merge_comp = integrate(read_data(ct3))
+
+    data = [tt_merge_comp, ct_merge_comp]
+
+    mpl.cmp(data, num=2, labels=["Tarantula", "Crosstab"], title="Comparison of Boundary Error", path=r'result.png')
 
 
 if __name__ == "__main__":
